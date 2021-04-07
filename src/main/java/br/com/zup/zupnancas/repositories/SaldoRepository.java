0c3826ea1f727cff583763af256f5b89a5f8dd73
Thiago Seus <thiago.seus@yahoo.com.br>
2021-04-07 15:50:59 -0300

    Criar repositório do saldo

---
+++ src/main/java/br/com/zup/zupnancas/repositories/SaldoRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.zupnancas.repositories;
+
+import br.com.zup.zupnancas.models.Saldo;
+import org.springframework.data.repository.CrudRepository;
+
+public class SaldoRepository extends CrudRepository<Saldo, String> {
+}
