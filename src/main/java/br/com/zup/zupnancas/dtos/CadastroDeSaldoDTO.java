20f4b81b44d395aa1cda2fef7d6fc4ace9d423a0
Thiago Seus <thiago.seus@yahoo.com.br>
2021-04-07 15:58:55 -0300

    Criar DTO para cadastro de um novo saldo

---
+++ src/main/java/br/com/zup/zupnancas/dtos/CadastroDeSaldoDTO.java
@@ -0,0 +1,51 @@
+package br.com.zup.zupnancas.dtos;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+public class CadastroDeSaldoDTO {
+    @CPF
+    @NotNull
+    private String cpf;
+
+    private double valor;
+
+    @PositiveOrZero
+    private double limite;
+
+    public CadastroDeSaldoDTO() {
+    }
+
+    public CadastroDeSaldoDTO(@CPF @NotNull String cpf, double valor, @PositiveOrZero double limite) {
+        this.cpf = cpf;
+        this.valor = valor;
+        this.limite = limite;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+}
