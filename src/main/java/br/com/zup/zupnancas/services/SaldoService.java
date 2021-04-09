dbf71c9a610f82a700be1c7751b21ceb9c3c0c49
Thiago Seus <thiago.seus@yahoo.com.br>
2021-04-09 13:42:37 -0300

    Adicionar métodos para pagar ou creditar no saldo

--- src/main/java/br/com/zup/zupnancas/services/SaldoService.java
+++ src/main/java/br/com/zup/zupnancas/services/SaldoService.java
@@ -1,8 +1,14 @@
 package br.com.zup.zupnancas.services;
 
+import br.com.zup.zupnancas.exceptions.SaldoInsuficienteException;
+import br.com.zup.zupnancas.exceptions.SaldoNaoEncontradoException;
+import br.com.zup.zupnancas.models.Conta;
+import br.com.zup.zupnancas.models.Credito;
 import br.com.zup.zupnancas.models.Saldo;
 import br.com.zup.zupnancas.repositories.SaldoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class SaldoService {
@@ -18,5 +24,43 @@ public class SaldoService {
 
     public Iterable <Saldo> listarTodosOsSaldos() {
         return saldoRepository.findAll();
+    }
+
+    public void pesquisarSaldoPeloCpf(String cpf) {
+        Optional <Saldo> optionalSaldo = saldoRepository.findById(cpf);
+
+        if (optionalSaldo.isPresent()) {
+            return optionalSaldo.get();
+        }
+
+        throw new SaldoNaoEncontradoException();
+    }
+
+    private void validarSePodePagarConta(Conta conta, Saldo saldo) {
+        Saldo saldo = pesquisarSaldoPeloCpf(conta.getSaldo().getCpf());
+
+        double dinheiroDisponivel = saldo.getValor() + saldo.getLimite();
+
+        if (conta.getValor() > dinheiroDisponivel) {
+            throw new SaldoInsuficienteException();
+        }
+    }
+
+    public Saldo pagar(String cpf, Conta conta) {
+        Saldo saldo = pesquisarSaldoPeloCpf(cpf);
+
+        validarSePodePagarConta(conta, saldo);
+
+        saldo.setValor(saldo.getValor() - conta.getValor());
+
+        return saldoRepository.save(saldo);
+    }
+
+    public void creditar(String cpf, Credito credito) {
+        Saldo saldo = pesquisarSaldoPeloCpf(cpf);
+
+        saldo.setValor(saldo.getValor() + credito.getValor());
+
+        return saldoRepository.save(saldo);
     }
 }
